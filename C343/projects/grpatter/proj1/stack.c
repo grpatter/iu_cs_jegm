dfe2ba285d7384e076a85864bf2b35d6cfe29ebe
grpatter <grpatter@localhost>
2009-09-15 20:40:05 -0400

    C343\projects\grpatter\proj1\calculator.c C343\projects\grpatter\proj1\stack.c C343\projects\grpatter\proj1\stack.h

--- C343/projects/grpatter/proj1/stack.c
+++ C343/projects/grpatter/proj1/stack.c
@@ -14,39 +14,39 @@ int TOP;
 int mem[MEMSIZE];
 
 void push(int datum){
-  if((++TOP) >= MEMSIZE){
+  if((++TOP) >= MEMSIZE){//if over memory size, do something
     //OVERFLOW();
     printf("Stack Overflow.\n");
-	exit(1);
+	exit(1);//exit with error
   }else{
-    mem[TOP] = datum;
+    mem[TOP] = datum; //point top to new val
   }
   return;
 }
 
 bool isEmptyStack(){
-  return TOP == -1;
+  return TOP == -1;//returns true or false
 }
 
 int peek(){
   if(isEmptyStack()){
     //UNDERFLOW();
     printf("Stack Underflow.\n");
-	exit(1);
+	exit(1);//exit with error
   }else{
-    return mem[TOP];
+    return mem[TOP];//give back current val, do not move top
   }
 }
 
 int pop(){
   int ans = peek();
   
-  if(isEmptyStack()){
+  if(isEmptyStack()){//if empty, we have a problem
     //UNDERFLOW();
     printf("Stack Underflow.\n");
-	exit(1);
+	exit(1);//exit with error
   }else{
-	return mem[TOP--];
+	return mem[TOP--];//make sure to decrement
   }
 }
 
@@ -56,13 +56,19 @@ void makeEmptyStack(){
 }
 
 void printstack(){
-	if(isEmptyStack()){
+	if(isEmptyStack()){//if at end, start the other side of this recursion
 		return;
 	}else{
-		int t = pop();
-		printf("%d\n",t);
-		printstack();
-		push(t);
+		int t = pop();//preserve value
+		printf("%d ",t);//print val
+		printstack();//add to recursion list
+		push(t);//add val back after we come back
 	}
 
+}
+
+void wipe_stack(){
+	while(!isEmptyStack()){//while not at end
+		pop();//pop the top off
+	}
 }
--- C343/projects/grpatter/proj1/stack.h
+++ C343/projects/grpatter/proj1/stack.h
@@ -1,6 +1,12 @@
+/*
+** grpatter
+** stack.h
+** 
+*/
 void push(int datum);
 int pop();
 void makeEmptyStack();
 bool isEmptyStack();
 int peek();
 void printstack();
+void wipe_stack();
