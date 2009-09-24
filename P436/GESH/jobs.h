50010df1d6b5447dc6edc15341c3e9c2444c695e
grpatter <grpatter@users.noreply.github.com>
2009-09-23 23:59:29 -0400

    P436\GESH\jobs.h P436\GESH\main.cpp

--- P436/GESH/jobs.h
+++ P436/GESH/jobs.h
@@ -1,17 +1,21 @@
+#include <iostream>
+#include <string>
 
 #define ARGS 10
 
 struct jobStore{
 	int job_n;
-	char *cmd_a[ARGS];
-	char *cmd_s;
+	//char *cmd_a[ARGS];
+	//char *cmd_s;
 	char *cmd_status;
-	struct jobStruct *jbs;
+	char *cmd_name;
+	//string cmd_location;
+	struct jobStruct *realJob;
 }; 
 
 typedef struct jobStruct{
 	char *cmd_path;
 	int argc;
 	char *argv[ARGS];
-	char cmd_suffix;
 }jobStruct; 
+
