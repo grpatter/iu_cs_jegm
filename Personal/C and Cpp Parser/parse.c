2a2d45eb4f7c5560a945d94a0e4e32f50ca46817
lostpunisher <lostpunisher>
2009-12-29 21:51:20 -0500

    Initial parse solution commit. Allows for tags and options on run and during execution.

---
+++ Personal/C and Cpp Parser/parse.c
@@ -0,0 +1,20 @@
+/*
+** Indiana University | parse.c
+** (c) Jonathan M. Stout
+*/
+
+/* EXAMPLE USAGE
+**
+** #include "parse.h"
+**
+** int main(argc, *argv[]) {
+** 
+** addOption('s');
+** addFlag("help");
+**
+** parseCommandArgs(argc, argv);
+**
+** char* get_value_of_s = getOption('s');
+** bool get_value_of_help = getFlag("help");
+**
+*/
